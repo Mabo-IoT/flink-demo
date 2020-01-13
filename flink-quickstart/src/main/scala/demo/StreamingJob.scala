f11df9ea46627f3e86ac9946d88243d0a14433e3
heathkang <heath.kang@foxmail.com>
2020-01-13 15:39:09 +0800

    scala compatible issue with pulsar-flink lib

--- flink-quickstart/src/main/scala/demo/StreamingJob.scala
+++ flink-quickstart/src/main/scala/demo/StreamingJob.scala
@@ -9,7 +9,9 @@ import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.time.Time
 // import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+// import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+// import io.streamnative.connectors.pulsar.PulsarSourceBuilder
+import org.apache.flink.streaming.connectors.pulsar.FlinkPulsarSource
 import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.api.common.typeinfo.TypeInformation
@@ -17,9 +19,9 @@ import org.apache.flink.api.common.typeinfo.TypeInformation
 
 
 object StreamingJob {
-  private val ZOOKEEPER_HOST = "zookeeper:2181"
-  private val KAFKA_BROKER = "kafka:9092"
-  private val TRANSACTION_GROUP = "transaction"
+  // private val ZOOKEEPER_HOST = "zookeeper:2181"
+  // private val KAFKA_BROKER = "kafka:9092"
+  // private val TRANSACTION_GROUP = "transaction"
 
   def main(args : Array[String]){
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -27,21 +29,28 @@ object StreamingJob {
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.enableCheckpointing(1000)
     env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
-
-    // configure Kafka consumer
-    val kafkaProps = new Properties()
-    kafkaProps.setProperty("zookeeper.connect", ZOOKEEPER_HOST)
-    kafkaProps.setProperty("bootstrap.servers", KAFKA_BROKER)
-    kafkaProps.setProperty("group.id", TRANSACTION_GROUP)
+    
+    // props.setProperty("service.url", "pulsar://standalone:6650")
+    // // props.setProperty("admin.url", "http://...")
+    // props.setProperty("partitionDiscoveryIntervalMillis", "5000")
+    // props.setProperty("startingOffsets", "earliest")
+    // props.setProperty("topic", "test")
+    
+    
+    val props = new Properties()
+    props.setProperty("service.url", "pulsar://standalone:6650")
+    props.setProperty("admin.url", "http://standalone:8080")
+    props.setProperty("partitionDiscoveryIntervalMillis", "5000")
+    props.setProperty("startingOffsets", "earliest")
+    props.setProperty("topic", "test")
+    val source = new FlinkPulsarSource(props)
+// you don't need to provide a type information to addSource since FlinkPulsarSource is ResultTypeQueryable
+    // val dataStream = env.addSource(source)(null)
+    val transaction = env.addSource(source)(null);
 
     //topic id is test ，schema is json
-    implicit val typeInfo = TypeInformation.of(classOf[org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode])
-    val kafkaConsumer = new FlinkKafkaConsumer("test", new JSONKeyValueDeserializationSchema(true), kafkaProps)
-    
-    val transaction = env
-      .addSource(
-        kafkaConsumer
-      )
+    // implicit val typeInfo = TypeInformation.of(classOf[org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode])
+    // val kafkaConsumer = new FlinkKafkaConsumer("test", new JSONKeyValueDeserializationSchema(true), kafkaProps)
    
     // 1. calculate status kpi
     transaction.print()
